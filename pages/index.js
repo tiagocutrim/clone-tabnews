6653c967f23b39b0be26796b7840603633195446
tiagocutrim <tiagoccutrim@gmail.com>
2025-11-18 18:08:26 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Tiago Cutrim & Ingrid Cutrim & Dante Cutrim</h1>
+    return <h1>Tiago Cutrim & Ingrid Cutrim & Dante Cutrim ❤️  Nós te amamos muito</h1>
 }
 
 export default Home;
